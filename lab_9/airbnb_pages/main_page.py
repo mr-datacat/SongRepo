7116e8091c0156e4f4753bb7403faea0a4474d4f
Mikhail Slavnikov <datacat401@gmail.com>
2022-11-24 04:40:53 +0300

    simplified test case 1 in lab_4.md and updated test case 2 in lab_9 because Airbnb has been updated

--- lab_9/airbnb_pages/main_page.py
+++ lab_9/airbnb_pages/main_page.py
@@ -10,10 +10,16 @@ class MainPage:
     def __init__(self, driver: webdriver):
         self.driver = driver
         self.first_residence_locator = (By.XPATH, "(//div/a[@tabindex='-1'])[1]")
+        self.close_popup_button_locator = (By.XPATH, "//button[@aria-label='Закрыть']")
 
     def open(self):
         self.driver.get('https://www.airbnb.ru/?enable_auto_translate=false&display_currency=EUR')
         sleep(7)
+        self.close_popup()
+
+    def close_popup(self):
+        WebDriverWait(self.driver, 30) \
+            .until(EC.element_to_be_clickable(self.close_popup_button_locator)).click()
 
     def open_first_residence(self) -> ResidencePage:
         WebDriverWait(self.driver, 30) \
